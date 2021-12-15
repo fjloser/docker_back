4880ddca892c7a450b903874089d03e5dd679fe2
fj <fangj@geoscene.cn>
2021-12-15 17:30:35 +0800

    feat: 项目初始化

---
+++ server.js
@@ -0,0 +1,17 @@
+let http = require('http')
+let users = [
+    {id: 1, name: 'fj'},
+    {id: 2, name: 'kf'}
+]
+
+let server = http.createServer(function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    if(req.url === '/api/users'){
+        res.end(JSON.stringify(users))
+    }else{
+        res.end('not found')
+    }
+})
+server.listen(3003, () => {
+    console.log('后端部署在3003端口')
+})
